744bacc9f4b2389b28262462f77bc8b858d1d07c
Kevin Zheng <Kevin.Zheng4783@gmail.com>
2019-03-20 22:32:42 -0400

    added Heroku port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const userRouter = require('./routes/users');
 
