50f32af4ad7eed66fc0517584fc90b82131d47e5
Kevin Zheng <Kevin.Zheng4783@gmail.com>
2019-04-10 20:49:22 -0400

    Added port

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const {app} = require('./app')
-const port = 8002;
-// process.env.PORT ||
+const port = process.env.PORT || 8002;
+
+app.get('/', (req, res) => {
+  res.json('Hello world');
+})
 
 app.listen(port, ()=>{
   console.log(`Listening. TrainerU on port: ${port}`)
